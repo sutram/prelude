abb447898ecbe6835be6dba3873d1323dc8c7d61
Mahesh Padmanabhan <sutram@gamil.com>
2022-04-14 07:49:11 -0600

    Get rid of annoying flycheck errors in elisp code

--- personal/preload/init.el
+++ personal/preload/init.el
@@ -1,1 +1,6 @@
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(with-eval-after-load 'flycheck
+  ;; (setq-default flycheck-emacs-lisp-initialize-packages t)
+  ;; (setq-default flycheck-disabled-checkers '(emacs-lisp emacs-lisp-checkdoc))
+  (setq flycheck-global-modes '(not emacs-lisp-mode))
+)
