61adf5b4c214b8d2155c866cdc59a9ce47f18942
Mahesh Padmanabhan <sutram@gmail.com>
2022-04-16 08:14:15 -0600

    Move roam assets

--- personal/prelude-org-roam.el
+++ personal/prelude-org-roam.el
@@ -20,15 +20,34 @@
                                 ("%<%Y-%m-%d>"))
          )))
 
-(defun my-org-roam-search (&optional with-types)
+(defun my-org/roam-search (&optional with-types)
   "Search in `default-directory' with RG.
 With WITH-TYPES, ask for file types to search in."
   (interactive "P")
   (mu-helm-rg org-roam-directory with-types))
+
+
+(defun my-org/get-roam-asset-name ()
+  (interactive)
+  (let ((fp
+         (if (derived-mode-p 'dired-mode)
+             (dired-get-file-for-visit)
+           (buffer-file-name))
+         ))
+    fp))
+
+(defun my-org/move-roam-asset ()
+  (interactive)
+  (let* ((fp (my-org/get-roam-asset-name))
+         (target (concat (file-name-as-directory org-roam-directory) "assets/"))
+         (tfp (concat (file-name-as-directory target) (file-name-nondirectory fp))))
+    (mkdir target t)
+    (copy-file fp target t)
+    tfp))
 
 (global-set-key (kbd "C-c m l") 'org-roam-buffer-toggle)
 (global-set-key (kbd "C-c m f") 'org-roam-node-find)
 (global-set-key (kbd "C-c m i") 'org-roam-node-insert)
 (global-set-key (kbd "C-c m t") 'org-roam-dailies-capture-today)
 (global-set-key (kbd "C-c m d") 'org-roam-dailies-capture-date)
-(global-set-key (kbd "C-c m s") 'my-org-roam-search)
+(global-set-key (kbd "C-c m s") 'my-org/roam-search)
